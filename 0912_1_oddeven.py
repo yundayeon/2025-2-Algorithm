c58ee4c3e2db3d7b2e00b36f520dbed18faef541
day <yunda0331@gmail.com>
2025-09-12 11:18:44 +0900

    방법4

--- 0912_1_oddeven.py
+++ 0912_1_oddeven.py
@@ -11,7 +11,7 @@ for i in range(101):
         b+=i
 
 print(f"짝수합: {a}, 홀수합: {b}")
-
+#방법1:
 def f1():
     evenSum = 0 ; oddSum = 0
     for i in range(101):
@@ -22,6 +22,7 @@ def f1():
             
     print(f"방법1 : 홀수합 {oddSum}, 짝수합 {evenSum}")
     
+#방법2:
 def f2():
     totalSum = 0
     evenSum = 0 ; oddSum = 0
@@ -31,7 +32,27 @@ def f2():
             evenSum += i
             
     print(f"방법2 : 홀수합 {totalSum-evenSum}, 짝수합 {evenSum}")
-
+    
+#방법3: 
+def f3():
+    evenSum = 0 ; oddSum = 0
+    for i in range(1,101,2):
+        oddSum += i
+    for i in range(0,101,2):
+        evenSum += i
+            
+    print(f"방법3 : 홀수합 {oddSum}, 짝수합 {evenSum}")
+    
+    
+#방법4: 등차수열 합
+def f4():
+    oddSum = 50*(1+99) // 2
+    evenSum = 50*(2+100) // 2
+            
+    print(f"방법4 : 홀수합 {oddSum}, 짝수합 {evenSum}")
+    
 if __name__ == "__main__":
     f1()
     f2()
+    f3()
+    f4()
